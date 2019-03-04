760ab0b1c92e5fd8951d9c225b25b621d0e4cab0
sijia3 <979337189@qq.com>
2019-03-04 11:25:50 +0800

    2019-03-04正常更新

--- WeightNet.py
+++ WeightNet.py
@@ -27,11 +27,12 @@ def create_placeholders(n_H0, n_W0, n_C0, n_y):
 
 
 # 初始化参数
-def initialize_parameters():
-    # tf.set_random_seed(1)  # so that your "random" numbers match ours
-    W1 = tf.get_variable("W1", [5, 5, 1, 6], initializer=tf.contrib.layers.xavier_initializer(seed=2))
-    W2 = tf.get_variable("W2", [5, 5, 6, 8], initializer=tf.contrib.layers.xavier_initializer(seed=2))
-    W3 = tf.get_variable("W3", [5, 5, 8, 16], initializer=tf.contrib.layers.xavier_initializer(seed=2))
+def initialize_parameters(randomSeed=1):
+
+    print("初始化参数的seed为"+str(randomSeed))
+    W1 = tf.get_variable("W1", [5, 5, 1, 6], initializer=tf.contrib.layers.xavier_initializer(seed=randomSeed))
+    W2 = tf.get_variable("W2", [5, 5, 6, 8], initializer=tf.contrib.layers.xavier_initializer(seed=randomSeed))
+    W3 = tf.get_variable("W3", [5, 5, 8, 16], initializer=tf.contrib.layers.xavier_initializer(seed=randomSeed))
     # W4 = tf.get_variable("W4", [2, 2, 16, 32], initializer=tf.contrib.layers.xavier_initializer(seed=0))
 
     parameters = {"W1": W1,
@@ -44,7 +45,8 @@ def initialize_parameters():
 
 
 # 前向传播
-def forward_propagation(X, parameters, num, isTrain=True, flag=0):
+def forward_propagation(X, parameters, num, isTrain=True, flag=0, randomSeed=1):
+    print("全连接层的seed为"+str(randomSeed))
     # Retrieve the parameters from the dictionary "parameters"
     W1 = parameters['W1']
     W2 = parameters['W2']
@@ -90,13 +92,13 @@ def forward_propagation(X, parameters, num, isTrain=True, flag=0):
     b1 = "bias1_"+str(flag)
     w2= "weight2_"+str(flag)
     b2 = "bias2_"+str(flag)
-    fc1_weights = tf.get_variable(w1, [nodes, 64], initializer=tf.truncated_normal_initializer(stddev=0.1, seed=1))
+    fc1_weights = tf.get_variable(w1, [nodes, 64], initializer=tf.truncated_normal_initializer(stddev=0.1, seed=randomSeed))
     fc1_biases = tf.get_variable(b1, [64], initializer=tf.constant_initializer(0.1))
     fc1 = tf.nn.relu(tf.matmul(reshaped, fc1_weights)+fc1_biases)
     # if isTrain:        # 防止过拟合
     #     fc1 = tf.nn.dropout(fc1, 0.66)
 
-    fc2_weights = tf.get_variable(w2, [64, 10], initializer=tf.truncated_normal_initializer(stddev=0.1, seed=1))
+    fc2_weights = tf.get_variable(w2, [64, 10], initializer=tf.truncated_normal_initializer(stddev=0.1, seed=randomSeed))
     fc2_biases = tf.get_variable(b2, [10], initializer=tf.constant_initializer(0.1))
     logit = (tf.matmul(fc1, fc2_weights)+fc2_biases)
     return logit, fc1_weights, fc2_weights
@@ -104,7 +106,7 @@ def forward_propagation(X, parameters, num, isTrain=True, flag=0):
 
 # 计算损失函数
 def compute_cost(Z3, Y):
-    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=Z3, labels=Y))
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=Z3, labels=Y))
     return cost
 
 
@@ -113,8 +115,9 @@ def model(X_train, Y_train, X_test, Y_test, learning_rate=0.0005, l2_rate=0.010,
     print("l2_rate="+str(l2_rate)+" and learning_rate="+str(learning_rate))
     ops.reset_default_graph()  # to be able to rerun the model without overwriting tf variables
     # tf.set_random_seed(1)  # to keep results consistent (tensorflow seed)
-    seed = 3  # to keep results consistent (numpy seed)
+
     (m, n_H0, n_W0, n_C0) = X_train.shape
+    a, b, c, d = X_test.shape
     m_test = X_test.shape[0]
     n_y = Y_train.shape[1]
     costs = []  # To keep track of the cost
@@ -122,36 +125,40 @@ def model(X_train, Y_train, X_test, Y_test, learning_rate=0.0005, l2_rate=0.010,
     num = tf.placeholder(tf.int32)
     flag = tf.Variable(0, trainable=False)
     X0, X1, X2, Y = create_placeholders(n_H0, n_W0, n_C0, n_y)
-    parameters = initialize_parameters()
-    Z0, fc1w0, fc2w0 = forward_propagation(X0, parameters, num, flag=0)
-    Z1, fc1w1, fc2w1 = forward_propagation(X1, parameters, num, flag=1)
-    Z2, fc1w2, fc2w2 = forward_propagation(X2, parameters, num, flag=2)
+    seed = 5
+    parameters = initialize_parameters(randomSeed=seed)
+    Z0, fc1w0, fc2w0 = forward_propagation(X0, parameters, num, flag=0, randomSeed=seed)
+    Z1, fc1w1, fc2w1 = forward_propagation(X1, parameters, num, flag=1, randomSeed=seed)
+    Z2, fc1w2, fc2w2 = forward_propagation(X2, parameters, num, flag=2, randomSeed=seed)
     cost0 = compute_cost(Z0, Y)
     cost1 = compute_cost(Z1, Y)
     cost2 = compute_cost(Z2, Y)
     # 采用L2正则化，避免过拟合
+    regularizer = tf.contrib.layers.l2_regularizer(l2_rate)
+    regularization0 = regularizer(fc1w0)+regularizer(fc2w0)
+    cost0 = cost0 + regularization0
+
     # regularizer = tf.contrib.layers.l2_regularizer(l2_rate)
-    # regularization = regularizer(fc1w0)+regularizer(fc2w0)
-    # cost = cost + regularization
-    global_step = tf.Variable(0, trainable=False)
-    learning_rate = tf.train.exponential_decay(learning_rate, global_step, 100, 0.96, staircase=False)
+    regularization1 = regularizer(fc1w1)+regularizer(fc2w1)
+    cost1 = cost1 + regularization1
+
+    # regularizer = tf.contrib.layers.l2_regularizer(l2_rate)
+    regularization2 = regularizer(fc1w2)+regularizer(fc2w2)
+    cost2 = cost2 + regularization2
+
+    global_step = tf.Variable(0, dtype=tf.int64, trainable=False)
+    learning_rate = tf.train.exponential_decay(learning_rate, global_step, 100, 0.96, staircase=True)
     optimizer0 = tf.train.AdamOptimizer(learning_rate).minimize(cost0, global_step)
     optimizer1 = tf.train.AdamOptimizer(learning_rate).minimize(cost1, global_step)
     optimizer2 = tf.train.AdamOptimizer(learning_rate).minimize(cost2, global_step)
-    normalize0 = tf.nn.l2_normalize(Z0, axis=1)
-    normalize1 = tf.nn.l2_normalize(Z1, axis=1)
-    normalize2 = tf.nn.l2_normalize(Z2, axis=1)
     init = tf.global_variables_initializer()
     saver = tf.train.Saver()
     with tf.Session() as sess:
         sess.run(init)
         for epoch in range(num_epochs):
-            # n_H0, n_W0, n_C0
             x0 = X_train[:,:,:,0].reshape(m, n_H0, n_W0, 1)
             x1 = X_train[:,:,:,1].reshape(m, n_H0, n_W0, 1)
             x2 = X_train[:,:,:,2].reshape(m, n_H0, n_W0, 1)
-
-            a,b,c,d = X_test.shape
             X0T = X_test[:,:,:,0].reshape(a, b, c, 1)
             X1T = X_test[:,:,:,1].reshape(a, b, c, 1)
             X2T = X_test[:,:,:,2].reshape(a, b, c, 1)
@@ -160,8 +167,9 @@ def model(X_train, Y_train, X_test, Y_test, learning_rate=0.0005, l2_rate=0.010,
             _, minibatch_cost2 = sess.run([optimizer2, cost2], feed_dict={X2: x2, Y: Y_train, num: m})
 
             if print_cost is True and epoch % 5 == 0:
-                print("Cost after epoch %i: %f" % (epoch, minibatch_cost0+minibatch_cost1+minibatch_cost2))
-                Z = Z0*0.4+Z1*0.4+Z2*0.2
+                print("损失函数经过%i次遍历后: %f" % (epoch, minibatch_cost0+minibatch_cost1+minibatch_cost2))
+                # print("Cost after epoch %i: %f" % (epoch, minibatch_cost0+minibatch_cost1+minibatch_cost2))
+                Z = Z0*0.6+Z1*0.2+Z2*0.2
                 predict_op = tf.argmax(Z, 1)  # 返回每行最大值的索引
                 correct_prediction = tf.equal(predict_op, tf.argmax(Y, 1))
                 accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
@@ -169,10 +177,10 @@ def model(X_train, Y_train, X_test, Y_test, learning_rate=0.0005, l2_rate=0.010,
                 test_accuracy = accuracy.eval({X0: X0T, X1: X1T,X2: X2T, Y: Y_test, num: m_test, isTrain: False})
                 print("训练集识别率:", train_accuracy)
                 print("测试集识别率:", test_accuracy)
-                # if save_session is True and test_accuracy > 0.874:
-                #     save_files = './session/model_forloop'+str(epoch)+'.ckpt'
-                #     saver.save(sess, save_files)
-                #     print("模型"+save_files+"保存成功.")
+                if save_session is True and test_accuracy > 0.877:
+                    save_files = './session/model_forloop'+str(epoch)+'.ckpt'
+                    saver.save(sess, save_files)
+                    print("模型"+save_files+"保存成功.")
             if print_cost is True and epoch % 1 == 0:
                 costs.append(minibatch_cost0+minibatch_cost1+minibatch_cost2)
         return parameters
